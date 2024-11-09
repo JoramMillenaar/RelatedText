0ba335067169811ac2e06130283cded0046af683
jofoks <joormillenaar@live.nl>
2024-11-08 17:47:18 -0800

    Added modified openAPI generated server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
 import express from 'express';
+import { Command } from 'commander';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const program = new Command();
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with ESM!');
-});
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const app = express()
+
+/* Embeddings */
+app.delete('/embeddings/:id', function (req, res) {
+	//TODO
+	res.status(503).send('Unimplemented')
+})
+app.delete('/embeddings/all', function (req, res) {
+	//TODO
+	res.status(503).send('Unimplemented')
+})
+app.post('/embeddings', function (req, res) {
+	//TODO
+	res.status(503).send('Unimplemented')
+})
+app.put('/embeddings/:id', function (req, res) {
+	//TODO
+	res.status(503).send('Unimplemented')
+})
+
+/* Similar */
+app.post('/similar', function (req, res) {
+	//TODO
+	res.status(503).send('Unimplemented')
+})
+
+program
+  .option('-p, --port <number>', 'port to listen on', '3000')
+  .on('command:*', (operands) => {
+    console.error(`Unknown option: ${operands.join(' ')}`);
+    process.exit(1);
+  });
+
+program.parse(process.argv);
+
+const options = program.opts();
+const port = options.port;
+
+const server = app.listen(port, function () {
+	const address = server.address()
+	if (typeof address === 'string') {
+		console.log(`Listening on ${ address }`)
+	} else if (address) {
+		console.log(`Listening on ${ address.port }`)
+	}
+})
